19216256edbe2a4905168228350a3badfc871665
Rahul <rahulkadam2247@gmail.com>
2025-09-01 22:50:39 +0100

    Deploy

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rahul\Desktop\msc final year project\Analyze_It\venv
+set VIRTUAL_ENV=C:\Users\rahul\Desktop\Analyze_It\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
