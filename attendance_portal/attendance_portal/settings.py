bf050094b12c5671f7b52f7fefc7aa1c10b034e5
Rahul <rahulkadam2247@gmail.com>
2025-09-01 14:31:59 +0100

    changes before deploying

--- attendance_portal/attendance_portal/settings.py
+++ attendance_portal/attendance_portal/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'attendance_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'analyzeit_db',           # Database name
+        'USER': 'user1',                # Username
+        'PASSWORD': 'user',        # Password
+        'HOST': 'localhost',             # Host
+        'PORT': '5432',                 
     }
 }
 
